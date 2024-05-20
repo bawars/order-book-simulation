c85db1904f340f2207d2390171ec52b79237b84a
Bawer <bawera@kth.se>
2024-05-20 17:48:14 +0200

    implemented some assertion tests

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
-from order_book import OrderBook, simulate_orders
+from gui_orderbook import OrderBookGUI
+from order_book import Order, OrderBook, generate_order
+import tkinter as tk
 
-def main():
 
-    enbok = OrderBook()
-    simulate_orders(enbok, duration=5, rate=3, mean_price=50, std_dev=2, quantity_range=(1, 20))
+def run_assertion_tests():
+    """function with which we run our testing"""
+
+    # testing our Order class
+    order = Order(side='buy', price=100, quantity=10)
+    assert order.side == 'buy'
+    assert order.price == 100
+    assert order.quantity == 10
+    assert order.id is not None
+    assert order.timestamp is not None
+    assert repr(order) == "(s: buy, p: 100, qty: 10)"
+    print("Order class tests passed")
+
+    # Test OrderBook class
+    order_book = OrderBook()
+    assert order_book is not None
+
+    # setting up a few orders to be tested in our order book
+    orders_setup = [
+        ('buy', 50, 5),
+        ('sell', 49, 5),
+        ('buy', 45, 10)
+    ]
+    orders = [Order(*order) for order in orders_setup]
+
+
+        ### add_order ###
+    order_book.add_order(orders[0])
+    assert len(order_book.bids) == 1
+    assert len(order_book.asks) == 0
+
+    order_book.add_order(orders[1])
+    assert len(order_book.bids) == 1
+    assert len(order_book.asks) == 1
+    print("OrderBook add_order tests passed")
+
+
+        ### testing query_book ####
+    top_of_book_1 = order_book.query_book()
+    assert top_of_book_1['Bid_side']['price'] == 50
+    assert top_of_book_1['Bid_side']['side'] == 'buy'
+    assert top_of_book_1['Ask_side']['qty'] == 5
+    print("OrderBook query_book tests passed")
+
+        ### testing match_order ###
+    order_book.add_order(orders[2])
+
+    matches = order_book.match_order()
+    assert len(matches) == 1
+    assert matches[0][1] == 49
+    assert matches[0][2] == 5
+
+    top_of_book = order_book.query_book()           # we now have a new 'top of book'
+    assert top_of_book['Bid_side']['price'] == 45
+    assert top_of_book['Ask_side']['qty'] == None   # ask side is now empty -> no matches available
+    matches = order_book.match_order()
+    assert len(matches) == 0
+    print("OrderBook match_order tests passed")
 
 
 
 if __name__ == '__main__':
-    main()
+    run_assertion_tests()
+    print("All tests passed successfully")
